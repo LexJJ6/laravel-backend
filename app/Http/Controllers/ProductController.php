08206a4b5f9b3a287eeb0d6ebff8b0c422237ece
João Pimenta <joaoalexcp1717@gmail.com>
2026-01-27 21:53:31 +0000

    feat: API, Produto, autenticação

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if(Auth::check())
+        {
+            $products = Product::withTrashed()->get();
+        }
+        else
+        {
+            $products = Product::all(); // não inclui os soft deleted como acima com withTrashed
+        }
+
+        return response()->json($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        if(Auth::check())
+        {
+            $product = Product::withTrashed()->find($id);
+        }
+        else
+        {
+            $product = Product::find($id);
+        }
+
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
