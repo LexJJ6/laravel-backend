ade21ba3608d14b15e6f275f7ba262e23d2c720e
João Pimenta <joaoalexcp1717@gmail.com>
2026-01-28 16:30:55 +0000

    refactor: Limpeza do ficheiro api.php

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,8 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Autenticação
-// Route::post('/login', [AuthController::class, 'login']);
-// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware(['web'])->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     Route::apiResource('/products', ProductController::class)->middleware('auth:sanctum');
 });
-
-// Produtos
-// Route::get('/products', [ProductController::class, 'index'])->middleware('optional_auth');
-// Route::post('/products', [ProductController::class, 'store'])->middleware('auth:sanctum');
-// Route::get('/products/{id}', [ProductController::class, 'show'])->middleware('optional_auth');
-// Route::put('/products/{id}', [ProductController::class, 'update'])->middleware('auth:sanctum');
-// Route::delete('/products/{id}', [ProductController::class, 'destroy'])->middleware('auth:sanctum');
-// Route::apiResource('/products', ProductController::class)->middleware('auth:sanctum');
